a8c1713a2403e402254f38fcda740431fcd58164
Dineshgupta79 <dinesh.gupta@miraeassetcm.com>
2024-10-03 13:04:02 +0530

    added swapNumbers

---
+++ Python/Java/swapNumbers.java
@@ -0,0 +1,23 @@
+package Python.Java;
+
+public class swapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.20f, second = 2.45f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        float temporary = first;
+
+        first = second;
+
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
